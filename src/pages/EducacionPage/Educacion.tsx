0150922ead7a042258666f6fb362e3a5e5930c8d
Soka_jplr <juanlazarorp@gmail.com>
2026-08-12 20:45:51 -0600

    Add page of Educacion

---
+++ src/pages/EducacionPage/Educacion.tsx
@@ -0,0 +1,32 @@
+function Educacion() {
+    return (
+        <>
+            <div>
+                <h1>Educación</h1>
+            </div>
+            <div>
+                <p>Licenciatura en tecnologías Computacionales</p>
+                <p>Estudios Concluidos en Octubre del 2025</p>
+            </div>
+            <div>
+                <img src="" alt="" />
+                <p>Universidad Veracruzana</p>
+                <p>Facultad de estadística e informática</p>
+
+            </div>
+
+            <div>
+                <h2>Titulo Universitario</h2>
+                <p>Ver Documento</p>
+                <a href="" download>Descargar documento</a>
+                <iframe
+                    src=""
+                    width="100%"
+                    height="600">
+                </iframe>
+            </div>
+        </>
+    )
+}
+
+export default Educacion
