61a09b3a7fea40a3f4c0889975063584bfde4a55
Soka_jplr <juanlazarorp@gmail.com>
2026-08-12 20:46:10 -0600

    Add page of sobre mi

---
+++ src/pages/SobreMiPage/SobreMi.tsx
@@ -0,0 +1,13 @@
+function SobreMi() {
+    return (
+        <>
+            <div><h1>Sobre Mí</h1></div>
+            <div>
+                <p>Hola, soy Juan, un desarrollador web apasionado por crear experiencias digitales únicas. Me especializo en el desarrollo full-stack, utilizando tecnologías modernas para construir aplicaciones web eficientes y atractivas.</p>
+                <p>Desde hace 2 años He echo proyectos que me han permitido desarrollar mis habilidades en desarrollo backend y frontend, costrución de API's, gestión de base de datos, mantenimiento de aplicaciones, manejo de gestor de versiones, creación de aplicaciones nativas, web, paginas web y desarrollo básico en aplicaciones móviles. </p>
+            </div>
+        </>
+    )
+}
+
+export default SobreMi
